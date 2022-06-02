7f901ca58e1c721fe09a0c30bfd99bb5b70ccdf0
Juan Freitas <juanfreitas094@gmail.com>
2022-06-02 16:16:22 -0300

    vue router installed

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,30 @@
 <template>
-  <RegistrationReview/>
-  <AttendanceAbout/>
-  <ProfessionalData/>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import RegistrationReview from './components/registration-review.vue'
-import AttendanceAbout from './components/attendance-about.vue'
-import ProfessionalData from './components/professional-data.vue'
-
-export default {
-  name: 'App',
-  components: {
-    RegistrationReview,
-    AttendanceAbout,
-    ProfessionalData
-}
-}
-</script>
-
 <style>
-#App {
-  font-family: 'Open Sans', sans-serif; 
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body{
-  background-color: #ffe766;
+nav {
+  padding: 30px;
 }
 
-.main-title {
-  font-family: 'Comfortaa', cursive;
-  color: #483698;
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-.btn-primary {
-  background-color: #483698;
-  font-family: 'Comfortaa', cursive;
-  border-color: #483698;
+nav a.router-link-exact-active {
+  color: #42b983;
 }
-
-.btn-primary:hover {
-  background-color: white;
-  color: #483698;
-  border-color: #483698;
-}
-
-.btn-primary:focus {
-  
-  background-color: white;
-  color: #483698;
-  border-color: #483698;
-}
-
-#progress-label {
-  font-family: 'Comfortaa', cursive;
-  color: #483698;
-}
-
-.progress-bar {
-  background-color: #483698;
-}
-
-.container {
-  background-color: #ffffff;
-}
-
-.form-control, .form-select {
-  border-color: #483698;
-}
-
 </style>
